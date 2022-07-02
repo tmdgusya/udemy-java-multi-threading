29508eee4b08db6c06492bc66dfde271299ca84e
tmdgusya <tmdgusya@naver.com>
2022-07-03 00:08:31 +0900

    How to Set Thread Name And Priority

--- src/main/java/section2/Example01.java
+++ src/main/java/section2/Example01.java
@@ -4,8 +4,15 @@ public class Example01 {
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(() -> {
             // 여기에 어떤 코드를 넣던 실행할때는 OS 가 새로운 스레드로 실행시켜주도록 스케쥴링함.
-            System.out.println("We are now in Thread : " + Thread.currentThread().getName());
+            System.out.println("We are now in Thread " + Thread.currentThread().getName());
+            System.out.println("Current Thread priority is " + Thread.currentThread().getPriority());
         });
+
+        // Hikari 같은 걸 보면 Thread 이름 붙이는데, 이 메소드를 이용하는듯 함.
+        thread.setName("New Worker Thread");
+
+        // 최대치로 설정
+        thread.setPriority(Thread.MAX_PRIORITY);
 
         System.out.println("Current Thread : " + Thread.currentThread().getName() + " before starting a new Thread");
         thread.start();
